f7238726dbbecadc95675a13fad217be231ac893
Leandro Medeiros <ordnaelmedeiros@gmail.com>
2023-02-23 09:31:21 -0300

    feat: refactor install

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,104 +1,19 @@
-# asdf install                            Install all the package versions listed
-#                                         in the .tool-versions file
-# asdf install <name>                     Install one tool at the version
-#                                         specified in the .tool-versions file
-# asdf install <name> <version>           Install a specific version of a package
 # asdf install <name> latest[:<version>]  Install the latest stable version of a
 #                                         package, or with optional version,
 #                                         install the latest stable version that
 #                                         begins with the given string
 
-# echo "name: $name"
-# echo "version: $version"
+$pluginManager = [AsdfPluginManager]::new()
 
 if ($name -and $version) {
-
-    if (Test-Path "$ASDF_HOME_INSTALLS\$name\$version") {
-        Write-Warning "$name $version already installed"
-    } else {
-
-        $path_download = "$ASDF_HOME_DOWNLOADS\$name\$version"
-        $path_install = "$ASDF_HOME_INSTALLS\$name\$version"
-        $path_install_tmp = "$ASDF_HOME_INSTALLS\$name\tmp"
-
-        if (Test-Path "$ASDF_HOME_PLUGINS\$name\get-url.ps1") {
-            $url = ."$ASDF_HOME_PLUGINS\$name\get-url.ps1"
-        } else {
-            $url = 
-                (
-                    Get-Content "$ASDF_HOME_PLUGINS\$name\versions.json"
-                        | ConvertFrom-Json
-                        | Where-Object { $_.name -eq $version }
-                ).url
-        }
-
-        if ($url) {
-            $config = Get-Content "$ASDF_HOME_PLUGINS\$name\config.json" | ConvertFrom-Json
-            $configFileType = $config.fileType
-            $configFolderPath = $config.folderPath
-            if (Test-Path $path_download) {
-                Remove-Item -Recurse -Path $path_download
-            }
-            New-Item -ItemType Directory -Path $path_download
-            
-            Invoke-WebRequest -Uri $url -OutFile "$path_download\file"
-            if (Test-Path $path_install) {
-                Remove-Item -Recurse -Path $path_install
-            }
-            if (Test-Path $path_install_tmp) {
-                Remove-Item -Recurse -Path $path_install_tmp
-            }
-            New-Item -ItemType Directory -Path $path_install
-            New-Item -ItemType Directory -Path $path_install_tmp
-
-            if ($configFileType -eq "folder") {
-                if ($url.EndsWith(".zip")) {
-                    Expand-Archive -LiteralPath "$path_download\file" -DestinationPath $path_install_tmp
-                    Copy-Item -Recurse -Path ${path_install_tmp}${configFolderPath}* -Destination "$path_install"
-                } elseif ($url.EndsWith(".tar.gz")) {
-                    tar -xvzf "$path_download\file" -C $path_install_tmp
-                    Copy-Item -Recurse -Path ${path_install_tmp}${configFolderPath}* -Destination "$path_install"
-                }
-            } else {
-                Copy-Item -Path "$path_download\file" -Destination "$path_install\file.${configFileType}"
-            }
-            if (Test-Path "$ASDF_HOME_PLUGINS\$name\install.ps1") {
-                ."$ASDF_HOME_PLUGINS\$name\install.ps1"
-            }
-            Remove-Item -Recurse -Path $path_install_tmp
-            Write-Output "$name - $version installed"
-
-        } else {
-            Write-Warning "$name - $version not found"
-        }
-
-    }
-
+    $pluginManager.plugin($name).version($version).install()
 } elseif ($name) {
-    if (Test-Path "$PWD\.win-tool-versions") {
-        Get-Content "$PWD\.win-tool-versions"
-            | Select-String -Pattern "^$name "
-            | ForEach-Object {
-                $pluginarray = $_.ToString().split(" ")
-                $p = $pluginarray[0]
-                $l = $pluginarray[1]
-                asdf plugin add $p
-                asdf install $p $l
-            }
-    } else {
-        Write-Warning "local config not found"
-    }
+    $item = $pluginManager.readByfile($PWD) | Where-Object { $_.plugin.name -eq $name }
+    $pluginManager.add($item.plugin.name)
+    $pluginManager.plugin($item.plugin.name).version($item.name).install()
 } else {
-    if (Test-Path "$PWD\.win-tool-versions") {
-        Get-Content "$PWD\.win-tool-versions" |
-            ForEach-Object {
-                $pluginarray = $_.split(" ")
-                $p = $pluginarray[0]
-                $l = $pluginarray[1]
-                asdf plugin add $p
-                asdf install $p $l
-            }
-    } else {
-        Write-Warning "local config not found"
+    foreach ($item in $pluginManager.readByfile($PWD)) {
+        $pluginManager.add($item.plugin.name)
+        $pluginManager.plugin($item.plugin.name).version($item.name).install()
     }
 }
--- src/classes.ps1
+++ src/classes.ps1
@@ -3,6 +3,7 @@ class AsdfStatics {
     static [string]$HOME
     static [string]$ASDF_HOME
     static [string]$ASDF_HOME_INSTALLS
+    static [string]$ASDF_HOME_DOWNLOADS
     static [string]$ASDF_HOME_PLUGINS
     static [string]$ASDF_HOME_LOCAL_REPO
 
@@ -12,6 +13,29 @@ class AsdfUtils {
 
     static [void] trace([string]$msg) {
         Write-Information $msg -InformationAction Continue
+    }
+
+    static [AsdfVersion[]] readByfile([string]$path) {
+
+        $pluginManager = [AsdfPluginManager]::new()
+
+        $versions = @()
+
+        if (!$path.EndsWith("\") -and !$path.EndsWith("/")) {
+            $path += "\"
+        }
+        $path += ".win-tool-versions"
+
+        if (Test-Path $path) {
+            foreach($line in (Get-Content "$path")) {
+                $itens = $line.split(" ")
+                $plugin = $pluginManager.plugin($itens[0])
+                $version = [AsdfVersion]::new($plugin, $itens[1])
+                $version.path = $path
+                $versions += $version
+            }
+        }
+        return $versions
     }
 
 }
@@ -83,6 +107,18 @@ class AsdfPluginManager {
         return $list
     }
 
+    [AsdfPlugin] plugin([string]$name) {
+        $item = $this.all() | Where-Object { $_.name -eq $name }
+        if (!$item) {
+            throw "Plugin ${name} not found"
+        }
+        return $item
+    }
+
+    [AsdfVersion[]] readByfile([string]$path) {
+        return [AsdfUtils]::readByfile($path)
+    }
+
 }
 
 class AsdfPlugin {
@@ -128,30 +164,30 @@ class AsdfPlugin {
 
     [AsdfVersion] current() {
         $version = [AsdfVersion]::new($this, "_______")
-        $content = Get-Content "$([AsdfStatics]::HOME)\.win-tool-versions"
-            | Select-String -Pattern "^$($this.name) "
-            | Out-String
-        $content = $content.Trim()
-        if ($content) {
-            $version.name = $content.split(" ")[1]
-            $version.path = "$([AsdfStatics]::HOME)\.win-tool-versions"
+        $other = [AsdfUtils]::readByfile([AsdfStatics]::HOME) | Where-Object { $_.plugin.name -eq $this.name }
+        if ($other) {
+            $version.name = $other.name
+            $version.path = $other.path
         }
         $versionspath = ""
         $array = $PWD.ToString().split("\")
         foreach ( $i in $array ) {
             $versionspath += $i + "\"
-            if (Test-Path "$versionspath.win-tool-versions") {
-                $content = Get-Content "$versionspath.win-tool-versions"
-                    | Select-String -Pattern "^$($this.name) "
-                    | Out-String
-                $content = $content.Trim()
-                if ($content) {
-                    $version.name = $content.split(" ")[1]
-                    $version.path = "$versionspath.win-tool-versions"
-                }
+            $other = [AsdfUtils]::readByfile($versionspath) | Where-Object { $_.plugin.name -eq $this.name }
+            if ($other) {
+                $version.name = $other.name
+                $version.path = $other.path
             }
         }
         return $version
+    }
+
+    [AsdfVersion] version([string]$version) {
+        $item = $this.all() | Where-Object { $_.name -eq $version }
+        if (!$item) {
+            throw "Version $($this.name) ${version} not found"
+        }
+        return $item
     }
 
 }
@@ -185,6 +221,71 @@ class AsdfVersion {
         Set-Content -Path $dest -Value $content
         Write-Output "$($this.plugin.name) $($this.name)" >> $dest
         $this.configEnv()
+    }
+
+    [void] install() {
+
+        $path_install = "$([AsdfStatics]::ASDF_HOME_INSTALLS)\$($this.plugin.name)\$($this.name)"
+        $path_install_tmp = "$([AsdfStatics]::ASDF_HOME_INSTALLS)\$($this.plugin.name)\$($this.name)-tmp"
+        $path_download = "$([AsdfStatics]::ASDF_HOME_DOWNLOADS)\$($this.plugin.name)\$($this.name)"
+
+        if (Test-Path $path_install) {
+            Write-Warning "$($this.plugin.name) $($this.name) already installed"
+            return
+        }
+
+        $url = $this.getUrl()
+        if (!$url) {
+            Write-Warning "$($this.plugin.name) $($this.name) not found"
+            return
+        }
+
+        $config = Get-Content "$([AsdfStatics]::ASDF_HOME_PLUGINS)\$($this.plugin.name)\config.json" | ConvertFrom-Json
+
+        $this.recreatePath($path_download)
+        $this.recreatePath($path_install_tmp)
+
+        Invoke-WebRequest -Resume -Uri $url -OutFile "$path_download\file"
+        
+        $this.recreatePath($path_install)
+
+        if ($config.fileType -eq "folder") {
+            if ($url.EndsWith(".zip")) {
+                Expand-Archive -LiteralPath "$path_download\file" -DestinationPath $path_install_tmp
+                Copy-Item -Recurse -Path "${path_install_tmp}$($config.folderPath)*" -Destination "$path_install"
+            } elseif ($url.EndsWith(".tar.gz")) {
+                tar -xvzf "$path_download\file" -C $path_install_tmp
+                Copy-Item -Recurse -Path "${path_install_tmp}$($config.folderPath)*" -Destination "$path_install"
+            }
+        } else {
+            Copy-Item -Path "$path_download\file" -Destination "$path_install\file.$($config.fileType)"
+        }
+        if (Test-Path "$([AsdfStatics]::ASDF_HOME_PLUGINS)\$($this.plugin.name)\install.ps1") {
+            ."$([AsdfStatics]::ASDF_HOME_PLUGINS)\$($this.plugin.name)\install.ps1"
+        }
+        Remove-Item -Recurse -Path $path_install_tmp
+        Write-Warning "$($this.plugin.name) $($this.name) installed"
+        
+    }
+
+    [string] getUrl() {
+        if (Test-Path "$([AsdfStatics]::ASDF_HOME_PLUGINS)\$($this.plugin.name)\get-url.ps1") {
+            $url = ."$([AsdfStatics]::ASDF_HOME_PLUGINS)\$($this.plugin.name)\get-url.ps1"
+        } else {
+            $url = (
+                    Get-Content "$([AsdfStatics]::ASDF_HOME_PLUGINS)\$($this.plugin.name)\versions.json"
+                        | ConvertFrom-Json
+                        | Where-Object { $_.name -eq $this.name }
+                ).url
+        }
+        return $url
+    }
+
+    [void] recreatePath([string]$dest) {
+        if (Test-Path $dest) {
+            Remove-Item -Recurse -Path $dest
+        }
+        New-Item -ItemType Directory -Path $dest
     }
 
 }
